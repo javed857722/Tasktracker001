87bf4d163c5ecd08c2a0953da705f22ec1a4b3dd
javed857722 <jj857722@gmail.com>
2026-07-09 21:19:19 +0545

    Add documentation for Comment entity

--- app/src/main/java/com/example/tasktracker001/data/Comment.kt
+++ app/src/main/java/com/example/tasktracker001/data/Comment.kt
@@ -3,6 +3,16 @@ package com.example.tasktracker001.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * Data class representing a comment on a task.
+ *
+ * @property id Unique ID for the comment.
+ * @property taskId The ID of the task this comment belongs to.
+ * @property userId The ID of the user who posted the comment.
+ * @property username The username of the user who posted the comment.
+ * @property text The content of the comment.
+ * @property timestamp The time the comment was created.
+ */
 @Entity(tableName = "comments")
 data class Comment(
     @PrimaryKey(autoGenerate = true)

bb69623ab6031bc493e06b085722322857dd1fe9
javed857722 <jj857722@gmail.com>
2026-07-09 21:16:51 +0545

    Add KDoc documentation to MainActivity

--- app/src/main/java/com/example/tasktracker001/MainActivity.kt
+++ app/src/main/java/com/example/tasktracker001/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.tasktracker001.ui.theme.JavedIqbalTaskTrackerTheme
 
+/**
+ * The main activity of the Task Tracker application.
+ * This activity serves as the entry point and sets up the Compose UI with the application theme.
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
